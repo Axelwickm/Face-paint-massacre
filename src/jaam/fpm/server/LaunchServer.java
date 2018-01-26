c19086cd0814990c750346444036e58462acc451
Axel Wickman <axelwickm@gmail.com>
2018-01-26 23:52:38 +0100

    Client manager

--- src/jaam/fpm/server/LaunchServer.java
+++ src/jaam/fpm/server/LaunchServer.java
@@ -10,39 +10,7 @@ import java.io.IOException;
 
 public class LaunchServer {
 	public static void main(String[] args) {
-		Server server = new Server();
-		Kryo kryo = server.getKryo();
-		kryo.register(TestPacket.class);
-
-		server.start();
-
-		try {
-			server.bind(54555, 54777);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-		server.addListener(new Listener(){
-			@Override
-			public void connected(Connection connection) {
-				super.connected(connection);
-				System.out.println(connection.getID());
-			}
-
-			@Override
-			public void disconnected(Connection connection) {
-				super.disconnected(connection);
-				System.out.println("Disconnected");
-			}
-
-			@Override
-			public void received(Connection connection, Object object){
-				System.out.println("Got packet");
-				if (object instanceof TestPacket){
-					System.out.println("TESTVALUE: "+((TestPacket) object).VAL);
-				}
-			}
-		});
+		ClientManager clientManager = new ClientManager();
 
 		while (true){
 
