ad48e6cd6285d19e870df78a96ebd544bb878c24
Axel Wickman <axelwickm@gmail.com>
2018-01-27 10:43:29 +0100

    Some velocity and death

--- src/jaam/fpm/server/Player.java
+++ src/jaam/fpm/server/Player.java
@@ -6,8 +6,26 @@ public class Player {
     private final int connection_id;
 
     private Vector2f position = new Vector2f();
+    private Vector2f velocity = new Vector2f();
+    private boolean dead;
 
-    public Player(int connection_id) {
+    public Player(int connection_id, Vector2f position) {
         this.connection_id = connection_id;
+        this.position = position;
+        this.velocity.set(0, 0);
+
+        dead = false;
+    }
+
+    public void update(double delta){
+
+    }
+
+    public void setVelocity(Vector2f velocity) {
+        this.velocity = velocity;
+    }
+
+    public Vector2f getVelocity() {
+        return velocity;
     }
 }
