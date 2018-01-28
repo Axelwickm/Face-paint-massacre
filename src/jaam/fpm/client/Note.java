7d10cb3008f00cb5e0f7841c737cb87d3ab0194d
Alfred Sporre <alfsp631@student.liu.se>
2018-01-28 15:15:04 +0100

    djaisd

--- src/jaam/fpm/client/Note.java
+++ src/jaam/fpm/client/Note.java
@@ -3,6 +3,7 @@ package jaam.fpm.client;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
+import org.newdawn.slick.ImageBuffer;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Note {
@@ -36,5 +37,21 @@ public class Note {
 
     public void setImage(Image image) {
         this.image = image;
+    }
+
+    public Image decodeImage(byte[] data, int width, int height) {
+        ImageBuffer ib = new ImageBuffer(width, height);
+        for (int i = 0; i < data.length; i += 4) {
+            int r = data[i + 0];
+            int g = data[i + 1];
+            int b = data[i + 2];
+            int a = data[i + 3];
+
+            System.out.println(r + " "+ g+ " " +b +" "+ a);
+
+            ib.setRGBA((i / 4) % width, Math.floorDiv((i / 4), width), r, g, b, a);
+        }
+        
+        return ib.getImage();
     }
 }
