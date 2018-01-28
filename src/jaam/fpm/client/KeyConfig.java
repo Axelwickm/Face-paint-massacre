f78cfc227cbb97631a5870dba1cda0a79077233e
Axel Wickman <axelwickm@gmail.com>
2018-01-28 12:09:35 +0100

    Started implementing undo mechanics

--- src/jaam/fpm/client/KeyConfig.java
+++ src/jaam/fpm/client/KeyConfig.java
@@ -15,12 +15,13 @@ public class KeyConfig {
 
 	public static final int NEXT_COLOR = Input.KEY_Q;
 	public static final int PREV_COLOR = Input.KEY_E;
+	public static final int UNDO = Input.KEY_Z;
 
 	public static final int START_DRAWING = Input.KEY_Q;
 	public static final int STOP_DRAWING = Input.KEY_R;
 
-	public static final int BIGGER_BRUSH = Input.KEY_Z;
-	public static final int SMALLER_BRUSH = Input.KEY_X;
+	public static final int BIGGER_BRUSH = Input.KEY_X;
+	public static final int SMALLER_BRUSH = Input.KEY_C;
 
 	private KeyConfig() { }
 }
